266c31d4fc65bd02865a6c402a847bb7d81c5e0d
shimar <shima01dev@gmail.com>
2020-01-18 11:19:50 +0900

    initialize via bundle gem.

---
+++ lib/bitly/client.rb
@@ -0,0 +1,8 @@
+require "bitly/client/version"
+
+module Bitly
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bitly/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bitly::Client do
+  it "has a version number" do
+    expect(Bitly::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
