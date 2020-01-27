ab84c7bde53923493b7aeb2b32b75de0d3b3594c
shimar <shima01dev@gmail.com>
2020-01-27 19:32:32 +0900

    enable to call shorten api via Bitly module.

--- lib/bitly.rb
+++ lib/bitly.rb
@@ -7,5 +7,23 @@ require 'bitly/v4'
 # Bitly module.
 module Bitly
   extend Config
-  class Error < StandardError; end
+
+  def self.shorten(long_url, options = {})
+    client.shorten(long_url, options)
+  end
+
+  class << self
+    private
+
+    def client
+      validate!
+      Bitly::V4::Client.new(access_token: access_token)
+    end
+
+    def validate!
+      raise StandardError, 'please set api_version.' unless api_version
+      raise StandardError, "#{api_version} is not supported." unless api_version == 'v4'
+      raise StandardError, 'please set access_token.' unless access_token
+    end
+  end
 end
--- spec/bitly_spec.rb
+++ spec/bitly_spec.rb
@@ -4,4 +4,54 @@ RSpec.describe Bitly do
   it 'has a version number' do
     expect(Bitly::VERSION).to eq '0.1.0'
   end
+
+  describe '#shorten' do
+    subject { described_class.shorten(long_url, options) }
+    let(:long_url) { 'https://www.google.com' }
+    let(:options) { {} }
+
+    context do
+      before do
+        stub_request(:post, 'https://api-ssl.bitly.com/v4/shorten')
+          .to_return(body: File.new('spec/fixtures/shorten_200.json'), status: 200)
+
+        described_class.configure do |config|
+          config.api_version = 'v4'
+          config.access_token = 'some token'
+        end
+      end
+
+      it { is_expected.to be_instance_of Bitly::V4::Bitlink }
+    end
+
+    context 'when api_version not set,' do
+      before do
+        described_class.configure do |config|
+          config.api_version = nil
+          config.access_token = 'some token'
+        end
+      end
+      it { expect { subject }.to raise_error('please set api_version.') }
+    end
+
+    context 'when api_version is not v4,' do
+      before do
+        described_class.configure do |config|
+          config.api_version = 'v3'
+          config.access_token = 'some token'
+        end
+      end
+      it { expect { subject }.to raise_error('v3 is not supported.') }
+    end
+
+    context 'when access_token not set,' do
+      before do
+        described_class.configure do |config|
+          config.api_version = 'v4'
+          config.access_token = nil
+        end
+      end
+      it { expect { subject }.to raise_error('please set access_token.') }
+    end
+  end
 end
